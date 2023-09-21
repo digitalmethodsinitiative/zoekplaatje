52e1de0ebae847f49c4a55e211937c062583aa2d
Stijn Peeters <stijn.peeters@uva.nl>
2023-09-21 11:19:42 +0200

    Support most Baidu widgets

--- modules/baidu.js
+++ modules/baidu.js
@@ -24,10 +24,10 @@ zoekplaatje.register_module(
         const parser = new DOMParser();
         let resultpage;
         let selectors = {
-            results: 'div.result',
+            results: '#content_left > div.result, #content_left > div.result-op, #content_left > .c-group-wrapper',
             title: 'h3',
             link: 'h3 a',
-            description: 'span[class*=content-right]'
+            description: 'span[class*=content-right], .c-span-last'
         };
 
         // check if file contains search results...
@@ -41,19 +41,119 @@ zoekplaatje.register_module(
             if(result_items) {
                 let query = decodeURI(path.split('wd=')[1].split('&')[0].split('#')[0]);
                 for (const item of result_items) {
-                    if(item.querySelector('h4') !== null) {
-                        continue;
-                    }
-                    const parsed_item = {
+                    let parsed_item = {
                         'id': now.format('x') + '-' + index,
                         'timestamp': now.format('YYYY-MM-DD hh:mm:ss'),
                         'source': domain,
                         'query': query,
                         'type': 'organic',
-                        'title': item.querySelector(selectors.title).innerText,
-                        'link': item.querySelector(selectors.link).getAttribute('href'),
-                        'description': item.querySelector(selectors.description).innerText
+                        'title': '',
+                        'link': '',
+                        'description': ''
                     };
+
+                    if(item.matches('.result')) {
+                        // organic result
+                        console.log(item)
+                        parsed_item = {...parsed_item,
+                            title: item.querySelector(selectors.title).innerText,
+                            link: item.querySelector(selectors.link).getAttribute('href'),
+                            description: item.querySelector(selectors.description).innerText
+                        }
+                    } else if(item.querySelector('div[class*=video-main-title]')) {
+                        parsed_item = {...parsed_item,
+                            type: 'video-widget',
+                            title: item.querySelector('h3').innerText,
+                            link: domain + item.querySelector('h3 a').getAttribute('href'),
+                            description: Array.from(item.querySelectorAll('div[class*=video-main-title]')).map(video => {
+                                return video.querySelector('a').innerText
+                            }).join(', ')
+                        }
+                    } else if(item.matches('div[tpl=recommend_list')) {
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'suggested-queries',
+                            title: item.querySelector('.c-font-medium').innerText,
+                            link: '',
+                            description: Array.from(item.querySelectorAll('a')).map(link => link.innerText).join(', ')
+                        }
+                    } else if (item.matches('div[tpl^=dict]')) {
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'dictionary-widget',
+                            title: item.querySelector(selectors.title).innerText,
+                            link: item.querySelector(selectors.link).getAttribute('href'),
+                            description: item.querySelector('td').innerText
+                        }
+                    } else if (item.matches('div[tpl*=yl_music_lrc]')) {
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'music-lyric-widget',
+                            title: item.querySelector(selectors.title).innerText,
+                            link: item.querySelector(selectors.link).getAttribute('href'),
+                            description: item.querySelector('div[class*=lrc-scroll]').innerText
+                        }
+                    } else if (item.matches('div[tpl*=img_normal]')) {
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'image-widget',
+                            title: item.querySelector(selectors.title).innerText,
+                            link: item.querySelector(selectors.link).getAttribute('href')
+                        }
+                    } else if (item.matches("div[mu*='baike.baidu']")) {
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'baike-wiki-widget',
+                            title: item.querySelector(selectors.title).innerText,
+                            link: item.querySelector(selectors.link).getAttribute('href')
+                        }
+                    } else if(item.matches('div[tpl*=tieba_general]')) {
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'tieba-widget',
+                            title: item.querySelector(selectors.title).innerText,
+                            link: item.querySelector(selectors.link).getAttribute('href')
+                        }
+                    } else if(item.matches('div[tpl*=_open_source_software')) {
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'software-widget',
+                            title: item.querySelector(selectors.title).innerText,
+                            link: item.querySelector(selectors.link).getAttribute('href')
+                        }
+                    } else if(item.matches('div[tpl*=game-page]')) {
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'game-info-widget',
+                            title: item.querySelector(selectors.title).innerText,
+                            link: item.querySelector(selectors.link).getAttribute('href')
+                        }
+                    } else if(item.matches('div[tpl*=news-realtime]')) {
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'news-widget',
+                            title: item.querySelector(selectors.title).innerText,
+                            link: item.querySelector(selectors.link).getAttribute('href')
+                        }
+                    } else if(item.matches('.c-group-wrapper')) {
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'misc-widget',
+                            title: item.querySelector('h2, h3, h4, *[class*=title]').innerText,
+                            link: item.querySelector('a').getAttribute('href')
+                        }
+                    } else if(item.matches('div[tpl*=wenda_abstract')) {
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'wenda-ai-widget',
+                            title: item.querySelector('h2, h3, h4, *[class*=title]').innerText,
+                            description: item.querySelector('div[class*=short-answer]').innerText,
+                            link: item.querySelector('a').getAttribute('href')
+                        }
+                    } else {
+                        console.log(item);
+                    }
+
                     index += 1;
                     results.push(parsed_item);
                 }
