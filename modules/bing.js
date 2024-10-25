fde3c64c85007bb69972b2b829efa1ac159becbe
Sal Hagen <s.h.hagen@uva.nl>
2024-10-25 17:10:31 +0200

    Recognise more Bing widgets and add column for interface section

--- modules/bing.js
+++ modules/bing.js
@@ -23,6 +23,33 @@ zoekplaatje.register_module(
             }
         }
 
+        function closest_parent(node, selector) {
+            while(node.parentNode) {
+                node = node.parentNode;
+                if (node instanceof HTMLElement) {
+                    if (node.matches(selector)) {
+                        return node;
+                    }
+                }
+            }
+            return null;
+        }
+
+        function text_from_childless_children(container) {
+            let text = '';
+
+            function traverse(node) {
+                if (node.nodeType === Node.TEXT_NODE) {
+                    text += node.textContent.trim() + ' ';
+                } else {
+                    node.childNodes.forEach(child => traverse(child));
+                }
+            }
+
+            traverse(container);
+            return text.trim();
+        }
+
         let results = [];
 
         // source_platform_url = URL in browser
@@ -45,7 +72,7 @@ zoekplaatje.register_module(
         const parser = new DOMParser();
         let resultpage;
         let selectors = {
-            results: '#b_results > li, #b_topw > li, #b_pole .sva_pole',
+            results: '#b_results > li, #b_topw > li, #b_pole .b_poleContent, #b_pole .sva_pole, #b_context > li:not(:has(.lite-entcard-blk)), #b_context .lite-entcard-blk, .b_top .b_widgetContainer',
             title: 'h2',
             link: 'h2 a',
             link_real: 'cite',
@@ -64,6 +91,7 @@ zoekplaatje.register_module(
                 let query = decodeURI(path.split('q=')[1].split('&')[0].split('#')[0]);
                 for (const item of result_items) {
                     const style = getComputedStyle(item);
+
                     if((style.visibility === 'hidden' || style.display === 'none' || parseInt(style.height) === 0)
                         || item.matches('#mfa_root')
                         || item.matches('.b_pag')
@@ -111,7 +139,7 @@ zoekplaatje.register_module(
                             results.push(ad_item);
                         }
                         continue;
-                    } else if(item.matches('.b_nwsAnsTopItem')) {
+                    } else if(item.matches('.b_nwsAns')) {
                         // news overview
                         parsed_item = {
                             ...parsed_item,
@@ -119,14 +147,14 @@ zoekplaatje.register_module(
                             title: item.querySelector(selectors.title).innerText,
                             link: item.querySelector(selectors.link).getAttribute('href'),
                             real_link: item.querySelector(selectors.link_real).innerText,
-                            description: Array.from(item.querySelectorAll('.na_t_news_caption')).map(headline => headline.innerText).join(', '),
+                            description: Array.from(item.querySelectorAll('.na_t_news_caption, .na_t')).map(headline => headline.innerText).join(', '),
                         }
                     } else if(item.matches('.b_imgans')) {
                         parsed_item = {
                             ...parsed_item,
                             type: 'image-widget',
                             title: item.querySelector(selectors.title).innerText,
-                            link: item.querySelector(selectors.link).getAttribute('href'),
+                            link: item.querySelector(selectors.link).getAttribute( 'href'),
                             real_link: item.querySelector(selectors.link).getAttribute('href')
                         }
                     } else if(item.matches('.b_vidAns') || item.querySelector('#mm_vidreco_cat')) {
@@ -137,12 +165,37 @@ zoekplaatje.register_module(
                             link: item.querySelector(selectors.link).getAttribute('href'),
                             real_link: item.querySelector(selectors.link).getAttribute('href')
                         }
-                    } else if(item.matches('.b_ans.b_mop') && item.querySelector('.df_alaskcarousel')) {
+                    } else if (item.matches('.b_ans.b_mop') && item.querySelector('.df_alaskcarousel')) {
                         parsed_item = {
                             ...parsed_item,
                             type: 'related-questions',
                             title: item.querySelector('.b_primtxt').innerText,
                             description: Array.from(item.querySelectorAll('.df_qntext')).map(question => question.innerText).join(', '),
+
+                        }
+                    } else if (item.matches('.b_dictans')) {
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'related-questions',
+                            title: item.querySelector('.dc_wt').innerText,
+                            description: Array.from(item.querySelectorAll('.dc_bld')).map(question => question.innerText).join(' '),
+                        }
+                    } else if (item.matches('.b_ans') && item.querySelector('.b_rs')) {
+                        // Related searches
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'related-searches',
+                            title: item.querySelector('h2').innerText,
+                            description: Array.from(item.querySelectorAll('.b_suggestionText')).map(question => question.innerText).join(', '),
+
+                        }
+                    } else if (item.matches('.b_ans') && item.querySelector('.b_rrsr')) {
+                        // Related searches in the sidebar
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'related-searches-sidebar',
+                            title: item.querySelector('h2').innerText,
+                            description: Array.from(item.querySelectorAll('.suggestion_text')).map(question => question.innerText).join(', '),
 
                         }
                     } else if(item.matches('.b_ans.b_mop') && item.querySelector('.sto_slides')) {
@@ -153,13 +206,41 @@ zoekplaatje.register_module(
                             title: item.querySelector('.sto_title').innerText,
                             description: Array.from(item.querySelectorAll('.sto_snippet')).map(snippet => snippet.innerText).join(' '),
                         }
-                    } else if(item.matches('.b_ans.b_mop') && item.querySelector('#placeAnswer')) {
+                    } else if(item.matches('.b_ans') && item.querySelector('#placeAnswer')) {
                         // 'travel info', info about a geographic location
                         parsed_item = {
                             ...parsed_item,
                             type: 'travel-widget',
                             title: item.querySelector('.hdr_ttl_lnk').innerText,
                             description: item.querySelector('.cityDesc1').innerText
+                        }
+                    } else if(item.matches('.b_ans.b_mop, .b_ans.b_top') &&
+                        item.querySelector('#lMapContainer, .b_lmLocal')) {
+                        // 'places info', info about hotels or restaurants
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'places-widget',
+                            title: item.querySelector('.b_ilhTitle').innerText,
+                            description: Array.from(item.querySelectorAll('.listCard, .lc_content')).map(snippet => snippet.innerText).join(' '),
+                        }
+                    } else if(item.matches('.b_ans.b_mop') &&
+                        item.querySelector('.l_ecrd_carousel')) {
+                        // 'hotel info', info about a hotel
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'carousel',
+                            title: item.querySelector('.l_ecrd_mttl').innerText,
+                            description: Array.from(item.querySelectorAll('.l_ecrd_itemdata')).map(snippet => snippet.innerText).join(' ')
+                        }
+                    } else if (item.matches('.b_ans') && item.querySelector('.b_remod')) {
+                        // 'Explore more' tab in the sidebar
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'explore-more',
+                            title: safe_prop(item.querySelector('.b_mgridTitle'), 'innerText'),
+                            description: Array.from(item.querySelectorAll('.epv_caption')).map(div => div.innerText.trim()).join(', '),
+                            link: '',
+                            real_link: '',
                         }
                     } else if(item.matches('.b_algo') || item.quer) {
                         // organic result
@@ -243,12 +324,127 @@ zoekplaatje.register_module(
                             link: '',
                             real_link: '',
                         }
-                    } else {
-                        console.log(item);
+                    } else if (item.querySelector('.b_wpTab')) {
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'page-subject',
+                            title: safe_prop(item.querySelector('.ent-dtab-txta'), 'innerText'),
+                            description: Array.from(item.querySelectorAll('.ent-dtab-btn')).map(div => div.innerText.trim()).join(', '),
+                            link: '',
+                            real_link: '',
+                        }
+                    } else if(item.querySelector('#b_wpt_creator')) {
+                        // Copilot answer
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'copilot-answer',
+                            title: '',
+                            link: '',
+                            real_link: '',
+                            description: '',
+                        }
+                    } else if(item.querySelector('#d_ans')) {
+                        // LLM answer
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'llm-answer',
+                            title: '',
+                            link: Array.from(item.querySelectorAll('.site span')).map(el => safe_prop(el, 'title').trim()).join(', '),
+                            real_link: '',
+                            description: safe_prop(item.querySelector('.df_con'), 'innerText'),
+                        }
+                    } else if(item.querySelector('.b_vPanel')) {
+                        // Topic card
+                        const title = safe_prop(item.querySelector('.title'), 'innerText')
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'topic-card',
+                            title: title,
+                            link: '',
+                            real_link: '',
+                            description: text_from_childless_children(item).replace(title, ''),
+                        }
+                    }  else if (item.matches(".lite-entcard-blk") && item.id.length > 0) {
+                        // Different knowledge graph sidebar sections.
+                        // Part of the same card but interesting enough to separate.
+
+                        // Dynamically attribute a type based on the element ID.
+                        let item_type = ''
+                        let item_id = item.id.split("_");
+                        item_id = item_id[item_id.length - 1];
+                        // Skip irrelevant or empty sections
+                        if (item_id === "Footer") {
+                            continue;
+                        }
+                        else if (item_id === "PlainHero" && (!item.querySelector('.b_snippet, .spl_logoheader'))) {
+                            continue
+                        }
+                        // to kebab-case
+                        item_type = item_id.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+                        if (!item_type) {
+                            item_type = 'unknown'
+                        }
+                        else if (item_type === 'plain_hero') {
+                            item_type = 'explanation-box'
+                        }
+
+                        const title = safe_prop(item.querySelector('h2, h3, .l_ecrd_bt_rings_ttl, .l_ecrd_txt_pln'), 'innerText');
+                        // Flexibly get the text content of the knowledge graph components, since these differ quite a
+                        // bit. Remove the first occurrence of the title, so we don't include it.
+                        const description = text_from_childless_children(item).replace(title, '');
+
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'knowledge-card-' + item_type,
+                            title: title,
+                            description: description,
+                            link: '',
+                            real_link: '',
+                        }
+                    } else if (item.matches('.b_ans') && item.querySelector('.data_module')) {
+                        // Misc right sidebar widget
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'misc-widget',
+                            title: safe_prop(item.querySelector('.title_text'), 'innerText'),
+                            description: '',
+                            link: '',
+                            real_link: '',
+                        }
+                    } else if (item.matches('.b_widgetContainer')) {
+                        // Left sidebar sliding in widget
+                        parsed_item = {
+                            ...parsed_item,
+                            type: 'left-sidebar-widget',
+                            title: safe_prop(item.querySelector('h2'), 'innerText'),
+                            description: Array.from(item.querySelectorAll('button, .rel_ent_t')).map(div => div.innerText.trim()).join(', '),
+                            link: '',
+                            real_link: '',
+                        }
                     }
+
+                    /* DETERMINE SECTION */
+                    // Left slide-in section
+                    if (item.matches('.b_widgetContainer')) {
+                        parsed_item['section'] = 'sidebar-left';
+                    }
+                    // Top
+                    else if (closest_parent(item, '#b_topw') || closest_parent(item, '#b_pole') ) {
+                        parsed_item['section'] = 'top';
+                    }
+                    // Right sidebar (usually part of knowledge graph)
+                    else if (closest_parent(item, '#b_context')) {
+                        parsed_item['section'] = 'sidebar-right';
+                    }
+                    // Main content
+                    else {
+                        parsed_item['section'] = 'main';
+                    }
+
                     index += 1;
                     parsed_item['real_link'] = !parsed_item['real_link'] || parsed_item['real_link'].indexOf('http') === 0 ? parsed_item['real_link'] : 'https://' + parsed_item['real_link'];
                     parsed_item['domain'] = parsed_item['real_link'].indexOf('http') === 0 ? parsed_item['real_link'].split('/')[2] : parsed_item['real_link'].split('/')[0];
+
                     results.push(parsed_item);
                 }
             }
